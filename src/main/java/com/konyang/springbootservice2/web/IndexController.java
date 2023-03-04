319635ec311a91d19c8522f58c31e85ea014f7c3
JackRo682 <60928974+JackRo682@users.noreply.github.com>
2023-03-04 10:54:08 +0900

    index.mustache에서 저장한 파일을 보여줄 수 있도록 처리하는 중

--- src/main/java/com/konyang/springbootservice2/web/IndexController.java
+++ src/main/java/com/konyang/springbootservice2/web/IndexController.java
@@ -1,13 +1,10 @@
 package com.konyang.springbootservice2.web;
 
 import com.konyang.springbootservice2.service.posts.PostsService;
-import com.konyang.springbootservice2.web.dto.PostsResponseDto;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,12 +15,5 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
-    }
-
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("posts", dto);
-        return "posts-update";
     }
 }
