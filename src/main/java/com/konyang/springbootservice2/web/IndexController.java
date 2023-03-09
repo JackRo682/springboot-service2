491cc060f9183f14bd023b787b6cbb078e7abaf1
JackRo682 <60928974+JackRo682@users.noreply.github.com>
2023-03-09 18:00:23 +0900

    IndexController 수정

--- src/main/java/com/konyang/springbootservice2/web/IndexController.java
+++ src/main/java/com/konyang/springbootservice2/web/IndexController.java
@@ -8,11 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
-    private final PostsService postsService;
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 }
